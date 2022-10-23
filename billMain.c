3985db1c88ca60a6bcea45e9453cbd9ed9f37f39
Dylan Oliva <djoliva@cpp.edu>
2022-10-23 15:15:45 -0700

    created outline

---
+++ billMain.c
@@ -0,0 +1,5 @@
+//first we need a method to randomly chose one of the four meals
+
+//after we need to take an input from the user for the tax rate and tip percent
+
+//lastly we need to then use the inputs to print out a bill with the item, tax amount, tip amount, and the total
